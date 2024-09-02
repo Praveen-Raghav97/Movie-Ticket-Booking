0091fa395821fde078c3acfe2bb85370fe23f92c
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-09-02 16:14:30 +0530

    d

--- server/app.js
+++ server/app.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:"https://movie-ticket-booking-ptxk-9f15bwjwl-praveen-raghavs-projects.vercel.app"
+    origin:"https://movie-ticket-booking-ptxk-9f15bwjwl-praveen-raghavs-projects.vercel.app",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  optionsSuccessStatus: 204
 }));
 
 //Middelwares 
