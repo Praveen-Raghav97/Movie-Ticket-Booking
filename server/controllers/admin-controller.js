38bfb91e8a0973d6415ed179c852df608d6847f9
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-09-02 10:54:26 +0530

    Bcypt to bcryptjs

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,5 +1,5 @@
 import Admin from "../models/Admin.js";
-import bcrypt from "bcrypt"
+import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 
 const registerAdmin = async(req,res,next) =>{
