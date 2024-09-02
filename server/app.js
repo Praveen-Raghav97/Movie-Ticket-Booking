9d80603ee4b7854c6fbe251fd1bfe76f234aa4b4
Praveen-Raghav97gir config --global user.email rishavthakur9027@gmail.com <158564215+Praveen-Raghav97@users.noreply.github.com>
2024-09-02 11:05:04 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin:["https://frontend-2kxfh67da-praveen-raghavs-projects.vercel.app"],
+        origin:[""],
         methods:["GET" , "POST" , "DELETE"],
         credentials:true,
     }
